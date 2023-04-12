79c65f7cd85efc74471b792eda5f66c2e56d73c6
Elzo <jaime@aeolant.com>
2023-04-12 14:01:55 -0600

    Cleanning Project

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -18,24 +18,19 @@ class RegisterController extends BaseController
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
+            'data1' => 'required',
+            'data2' => 'required',
+            'url' => 'required',
         ]);
    
         if($validator->fails()){
             return $this->sendError('Validation Error.', $validator->errors());       
         }
    
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $input['role_id'] = 3; 
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-        $success['name'] =  $user->name;
-   
-        return $this->sendResponse($success, 'User register successfully.');
+        // TODO Crear un folio de seguimiento para el concursante. 
+
+        
+        return $this->sendResponse($success, 'Contestant register successfully.');
     }
    
     /**
