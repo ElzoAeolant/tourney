5c48ba9eb471f76e687ed14c9dd6e683d42775cb
ElzoAeolant <jaime@aeolant.com>
2023-04-27 00:42:30 -0600

    change favicon and text

--- resources/views/layouts/footer/nav.blade.php
+++ resources/views/layouts/footer/nav.blade.php
@@ -1,23 +1,12 @@
 <footer class="footer">
     <div class="container">
         <nav>
-            <ul class="footer-menu">
-                <li>
-                    <a href="#" class="nav-link" target="_blank">{{ __('Kpsule4IT') }}</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link" target="_blank">{{ __('Tourney') }}</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link" target="_blank">{{ __('About Us') }}</a>
-                </li>
-            </ul>
             <p class="copyright text-center">
                 ©
                 <script>
                     document.write(new Date().getFullYear())
                 </script>
-                <a href="https://www.pescaypetroleo.info/">{{ __('Kpsule4IT') }}</a> &amp; <a href="https://www.pescaypetroleo.info/">{{ __('PePeBots') }}</a> {{ __(' Tourney') }}
+                &amp; <a href="https://www.pescaypetroleo.info/pepebots">{{ __('PePeBots') }}</a> {{ __(' Torneo') }}
             </p>
         </nav>
     </div>
