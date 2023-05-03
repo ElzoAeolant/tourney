77d472dce5b9c0664c724966b5cc6b5c0f425c5e
OscarRuizAEOLANT <oscar@aeolant.com>
2023-05-03 12:17:32 -0600

    Update radio buttons group

--- resources/views/participants/edit.blade.php
+++ resources/views/participants/edit.blade.php
@@ -79,18 +79,30 @@
                             </div>
                         </fieldset>
                         <fieldset>
-                            <div class="col-sm-10" class="form-group">
+                            <div class="col-sm-10 form-group">
                                 <input type="hidden" name="validated" value="{{ $participant->validated }}">
-                                <input type="checkbox" class="form-check-input{{ $errors->has('validated') ? ' is-invalid' : '' }}" name="validated" value="1" {{ $participant->validated ? 'checked' : '' }}>
-                                <label class="form-check-label" for="check2">Validar</label>
+                                <div class="form-check">
+                                    <input type="radio" class="form-check-input{{ $errors->has('validated') ? ' is-invalid' : '' }}" name="validated" value="1" {{ $participant->validated ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="validated-option-1">Validado</label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="radio" class="form-check-input{{ $errors->has('validated') ? ' is-invalid' : '' }}" name="validated" value="0" {{ !$participant->validated ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="validated-option-2">No validado</label>
+                                </div>
                                 @include('alerts.feedback', ['field' => 'validated'])
                             </div>
                         </fieldset>
                         <fieldset>
-                            <div class="col-sm-10" class="form-group">
+                            <div class="col-sm-10 form-group">
                                 <input type="hidden" name="showed" value="{{ $participant->showed }}">
-                                <input type="checkbox" class="form-check-input{{ $errors->has('showed') ? ' is-invalid' : '' }}" name="showed" value="1" {{ $participant->showed ? 'checked' : '' }}>
-                                <label class="form-check-label" for="check2">Participa</label>
+                                <div class="form-check">
+                                    <input type="radio" class="form-check-input{{ $errors->has('showed') ? ' is-invalid' : '' }}" name="showed" value="1" {{ $participant->showed ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="showed-option-1">Partipa</label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="radio" class="form-check-input{{ $errors->has('showed') ? ' is-invalid' : '' }}" name="showed" value="0" {{ !$participant->showed ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="showed-option-2">No participa</label>
+                                </div>
                                 @include('alerts.feedback', ['field' => 'showed'])
                             </div>
                         </fieldset>
